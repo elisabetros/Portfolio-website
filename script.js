65de10452d8d28308fdc15bddb370034c9074d15
elisabetros <32452401+elisabetros@users.noreply.github.com>
2018-01-07 19:58:11 +0100

    Updates and project, about and behind html files

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let slideIndex = 1;
-showSlides(slideIndex);
-
 let style= document.querySelector(".styleweb");
 let redesign= document.querySelector(".redesign");
 let simple= document.querySelector(".simple");
@@ -12,14 +9,7 @@ let projecttwo= document.querySelector(".projecttwo");
 let digital= document.querySelector(".digital");
 let story= document.querySelector(".story");
 let city= document.querySelector(".city");
-let logoTransfer= document.getElementById("front");
 
-
-window.addEventListener("scroll", moveIt);
-
-function moveIt(){
-  logoTransfer.classList.add("moveit");
-}
 
 
 style.addEventListener("mouseover", changeToStyle);
@@ -28,122 +18,94 @@ style.setAttribute("style","background-image:url('img/stylewe.png');");
 
 } style.addEventListener("mouseleave", changeBack);
 function changeBack(){
-style.setAttribute("style","background-image:url('img/prufa8.svg');");
+style.setAttribute("style","background-image:url('img/style2.png');");
 }
 
 redesign.addEventListener("mouseover", changeToRe);
 function changeToRe(){
-    console.log('working');
-redesign.setAttribute("style","background-image:url('img/redes.png'); filter: opacity(0.5);");
+redesign.setAttribute("style","background-image:url('img/redes.png');");
 }
 redesign.addEventListener("mouseleave", changeBacks);
 function changeBacks(){
- redesign.setAttribute("style","background-image:url('img/prufa8.svg');");
-    console.log("working");
+ redesign.setAttribute("style","background-image:url('img/redes2.png');");
 }
 
 simple.addEventListener("mouseover", changeToSi);
 function changeToSi(){
-simple.setAttribute("style","background-image:url('img/simp.png'); width:100%; height:100%; filter: opacity(.5);");
+simple.setAttribute("style","background-image:url('img/simp.png');");
 }
  simple.addEventListener("mouseleave", changeBackss);
 function changeBackss(){
-simple.setAttribute("style","background-image:url('img/prufa8.svg');"); 
+simple.setAttribute("style","background-image:url('img/simp2.png');"); 
 }
 
 sprite.addEventListener("mouseover", changeToSp);
 function changeToSp(){
-sprite.setAttribute("style","background-image:url('img/spri.png'); width:100%; height:100%; filter: opacity(.5);");
+sprite.setAttribute("style","background-image:url('img/spri.png');");
 }
  sprite.addEventListener("mouseleave", changeBacksss);
 function changeBacksss(){
-sprite.setAttribute("style","background-image:url('img/prufa8.svg');"); 
+sprite.setAttribute("style","background-image:url('img/spri2.png');"); 
 }
 
 interanim.addEventListener("mouseover", changeToIn);
 function changeToIn(){
-interanim.setAttribute("style","background-image:url('img/inter.png'); width:100%; height:100%; filter:opacity(.5);");
+interanim.setAttribute("style","background-image:url('img/inter.png');");
 }
  interanim.addEventListener("mouseleave", changeBacks4);
 function changeBacks4(){
-interanim.setAttribute("style","background-image:url('img/prufa8.svg');"); 
+interanim.setAttribute("style","background-image:url('img/inter2.png');"); 
 }
 
 intertrail.addEventListener("mouseover", changeToI);
 function changeToI(){
-intertrail.setAttribute("style","background-image:url('img/touch.png'); width:100%; height:100%; filter:opacity(0.5);");
+intertrail.setAttribute("style","background-image:url('img/touch.png');");
 }
  intertrail.addEventListener("mouseleave", changeBacks5);
 function changeBacks5(){
-intertrail.setAttribute("style","background-image:url('img/prufa8.svg');"); 
+intertrail.setAttribute("style","background-image:url('img/touch2.png')"); 
 }
 projectone.addEventListener("mouseover", changeToP1);
 function changeToP1(){
-projectone.setAttribute("style","background-image:url('img/pone.png'); width:100%; height:100%; filter: grayscale(100%);");
+projectone.setAttribute("style","background-image:url('img/pone.png');");
 }
  projectone.addEventListener("mouseleave", changeBacks6);
 function changeBacks6(){
-projectone.setAttribute("style","background-image:url('img/prufa8.svg');"); 
+projectone.setAttribute("style","background-image:url('img/pone2.png');"); 
 }
 
 projecttwo.addEventListener("mouseover", changeToP2);
 function changeToP2(){
-projecttwo.setAttribute("style","background-image:url('img/ptwo.png'); filter: grayscale(100%); width:100%; height:100%;filter:opacity(.5);");
+projecttwo.setAttribute("style","background-image:url('img/ptwo.png');;");
 }
  projecttwo.addEventListener("mouseleave", changeBacks7);
 function changeBacks7(){
-projecttwo.setAttribute("style","background-image:url('img/prufa8.svg');"); 
+projecttwo.setAttribute("style","background-image:url('img/ptwo2.png');"); 
 }
 
 digital.addEventListener("mouseover", changeToDi);
 function changeToDi(){
-digital.setAttribute("style","background-image:url('img/hina.png'); width:100%; height:100%; filter: opacity(.5);");
+digital.setAttribute("style","background-image:url('img/hina.png');");
 }
  digital.addEventListener("mouseleave", changeBack8);
 function changeBack8(){
-digital.setAttribute("style","background-image:url('img/prufa8.svg');"); 
+digital.setAttribute("style","background-image:url('img/hina2.png');"); 
 }
 
 story.addEventListener("mouseover", changeToSt);
 function changeToSt(){
-story.setAttribute("style","background-image:url('img/story-no.png'); width:100%; height:100%; filter:opacity(.5);");
+story.setAttribute("style","background-image:url('img/story-no.png');");
 }
  story.addEventListener("mouseleave", changeBacks9);
 function changeBacks9(){
-story.setAttribute("style","background-image:url('img/prufa8.svg');"); 
+story.setAttribute("style","background-image:url('img/story-no2.png');"); 
 }
 
 city.addEventListener("mouseover", changeToCi);
 function changeToCi(){
-city.setAttribute("style","background-image:url('img/two-lakes.png'); width:100%; height:100%; filter:opacity(.5);");
+city.setAttribute("style","background-image:url('img/two-lakes.png');");
 }
  city.addEventListener("mouseleave", changeBacks10);
 function changeBacks10(){
-city.setAttribute("style","background-image:url('img/prufa8.svg');"); 
-}
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display ="block"; 
-  dots[slideIndex-1].className += (" active");
+city.setAttribute("style","background-image:url('img/two-lakes2.png');"); 
 }
